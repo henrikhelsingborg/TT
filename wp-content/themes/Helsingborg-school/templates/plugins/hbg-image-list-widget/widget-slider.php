4c1ec141ce06d68e01c30e5d2e5711b5082e19ff
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-06-08 10:48:33 +0200

    Widget style for image list and inherit widget

--- wp-content/themes/Helsingborg-school/templates/plugins/hbg-image-list-widget/widget-slider.php
+++ wp-content/themes/Helsingborg-school/templates/plugins/hbg-image-list-widget/widget-slider.php
@@ -1,3 +1,4 @@
+<?php if ($args['id'] == 'slider-area') : ?>
 <div class="slider-container">
     <div class="slider-columns columns large-12 no-margin">
         <ul class="hero-orbit" data-orbit data-options="animation:fade; timer_speed:10000; slide_number:false;">
@@ -33,3 +34,38 @@
         </ul>
     </div>
 </div>
+<?php else : ?>
+<?php echo $before_widget; ?>
+    <ul class="orbit-slider" data-orbit data-options="animation:fade; timer_speed:10000; slide_number:false;">
+
+        <?php
+            foreach ($items as $num => $item) :
+                $force_width  = (!empty($item_force_widths[$num])) ? 'width:100%;' : '';
+                $force_margin = (!empty($item_force_margins[$num]) && !empty($item_force_margin_values[$num])) ? ' margin-top:-' . $item_force_margin_values[$num] . 'px;' : '';
+        ?>
+        <li style="background-image:url('<?php echo $item_imageurl[$num]; ?>');">
+            <?php if (!empty($item_texts[$num]) && !empty($item_links[$num])) : ?>
+            <div class="caption">
+                <div class="caption-content">
+                    <div class="row">
+                        <div class="columns large-12">
+                            <?php
+                                if (!empty($item_texts[$num])) {
+                                    echo $item_texts[$num];
+                                }
+
+                                if (!empty($item_links[$num])) :
+                            ?>
+                                <a href="<?php echo $item_links[$num]; ?>" class="read-more">Läs mer</a>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+        </li>
+        <?php endforeach; ?>
+
+    </ul>
+<?php echo $after_widget; ?>
+<?php endif; ?>
