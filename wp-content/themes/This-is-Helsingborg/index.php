7bb79f6769cdbccf5e7e5beaf7c49ed2355ea744
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-06-16 13:37:10 +0200

    News section

--- wp-content/themes/This-is-Helsingborg/index.php
+++ wp-content/themes/This-is-Helsingborg/index.php
@@ -15,12 +15,12 @@
 
     <meta name="google-translate-customization" content="10edc883cb199c91-cbfc59690263b16d-gf15574b8983c6459-12">
 
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/favicon.ico" type="image/x-icon">
 
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/apple-touch-icon-144x144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/apple-touch-icon-114x114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/apple-touch-icon-72x72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/apple-touch-icon-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-precomposed.png">
 
     <?php wp_head(); ?>
 </head>
@@ -59,7 +59,7 @@
             </form>
         </div>
 
-        <section class="section-featured">
+        <section class="section-featured creamy">
             <div class="container">
                 <div class="row">
 
@@ -111,10 +111,130 @@
 
         <section class="section-news">
             <div class="container">
+
                 <div class="row">
                     <div class="columns large-12">
                         <h2 class="text-magenta"><i class="fa fa-newspaper-o"></i> Aktuellt i Helsingborg</h2>
                     </div>
+
+                    <div class="index">
+                        <div class="columns large-4 medium-6">
+                            <a href="#" class="index-item">
+                                <img src="http://www.helsingborg.se/wp-content/uploads/2014/12/nsr_avfallskarl_300x200_foto_NSR_Rickard_Johansson_Studio-E.jpg">
+                                <span class="index-caption">Ändrade dagar för sophämtning i midsommar</span>
+                            </a>
+                        </div>
+                        <div class="columns large-4 medium-6">
+                            <a href="#" class="index-item">
+                                <img src="http://www.helsingborg.se/wp-content/uploads/2015/01/sommarlov-nyhet.jpg">
+                                <span class="index-caption">Sommarlovsprogrammet är här</span>
+                            </a>
+                        </div>
+                        <div class="columns large-4 medium-6">
+                            <a href="#" class="index-item">
+                                <img src="http://www.helsingborg.se/wp-content/uploads/2015/06/kommunfullmaktige_300.jpg">
+                                <span class="index-caption">Kommunfullmäktige beslutar om budgeten</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="columns large-6 medium-6">
+                        <div class="box box-outlined">
+                            <h3>Nyheter</h3>
+                            <div class="box-content">
+                                <ul class="list list-links">
+                                    <li><a href="#" class="link-item">Välavägen avstängd från 15 juni</a></li>
+                                    <li><a href="#" class="link-item">Trafiken i korsning på Malmöleden påverkas från måndag 15 juni</a></li>
+                                    <li><a href="#" class="link-item">Snart har du tidningen Vår stad i din brevlåda</a></li>
+                                    <li><a href="#" class="link-item">Upplev Helsingborgs vattenliv i sommar</a></li>
+                                    <li><a href="#" class="link-item">Busslinjer får nya körvägar när sommartidtabellen börjar gälla</a></li>
+                                    <li><a href="#" class="link-item">Följ med på båttur med Sabella och titta på Öresunds bottendjur</a></li>
+                                    <li><a href="#" class="link-item">Energisnålt byggande kan premieras</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="columns large-6 medium-6">
+                        <div class="box box-outlined">
+                            <h3>Evenemang</h3>
+                            <div class="box-content">
+                                <ul class="list list-events">
+                                    <li>
+                                        <a href="#" class="event-item featured">
+                                            <div class="columns large-4 medium-4 small-12 featured-image">
+                                                <img src="http://www.fredriksdal.se/ImageVaultFiles/id_54706/cf_127/480x270-fredriksdal140_RalfEkvall.PNG" class="responsive">
+                                            </div>
+                                            <div class="columns large-8 medium-8 small-12">
+                                                Välkommen till Helsingborgs officiella nationaldagsfirande på Fredriksdal den 6 juni.
+                                                <span class="link-item link-item-light">Läs mer och se programmet här</span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="event-item">
+                                            <span class="date">
+                                                <span class="date-day"><strong>Idag</strong></span>
+                                                <span class="date-time">10:00</span>
+                                            </span>
+                                            <span class="title link-item">Sommarteater i Det Gröna</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="event-item">
+                                            <span class="date">
+                                                <span class="date-day"><strong>Idag</strong></span>
+                                                <span class="date-time">11:00</span>
+                                            </span>
+                                            <span class="title link-item">Årets utställningar på Råå museum</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="event-item">
+                                            <span class="date">
+                                                <span class="date-day"><strong>Idag</strong></span>
+                                                <span class="date-time">12:00</span>
+                                            </span>
+                                            <span class="title link-item">"Bomber o Granater" - en gästutställning med Tintin och hans spännande värld på Toy World</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="event-item">
+                                            <span class="date">
+                                                <span class="date-day"><strong>Idag</strong></span>
+                                                <span class="date-time">14:00</span>
+                                            </span>
+                                            <span class="title link-item">Sommarteater i Det Gröna</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="event-item">
+                                            <span class="date">
+                                                <span class="date-day">17</span>
+                                                <span class="date-month">juni</span>
+                                            </span>
+                                            <span class="title link-item">Sommarteater i Det Gröna</span>
+                                        </a>
+                                    </li>
+                                    <li><a href="#" class="list-more">Fler evenemang</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="creamy">
+            <div class="container">
+                <div class="row">
+                    <div class="columns large-4">Test</div>
+                    <div class="columns large-4">Test</div>
+                    <div class="columns large-4">Test</div>
                 </div>
             </div>
         </section>
