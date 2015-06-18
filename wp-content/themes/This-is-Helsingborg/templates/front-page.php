67b4cb8f5d7bb26252fbacd30206a3f1534451b9
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-06-18 13:03:58 +0200

    Front page template

---
+++ wp-content/themes/This-is-Helsingborg/templates/front-page.php
@@ -0,0 +1,179 @@
+<?php
+    /*
+    Template Name: Start
+    */
+
+    get_header();
+?>
+
+<section class="section-featured creamy">
+    <div class="container">
+        <div class="row">
+
+            <div class="columns large-6">
+                <div class="box widget">
+                    <h3><i class="fa fa-dot-circle-o"></i> Genvägar</h3>
+                    <div class="box-content">
+                        <ul class="list list-links">
+                            <li><a class="link-item" href="#">Sommarlovsaktiviteter 2015</a></li>
+                            <li><a class="link-item" href="#">Ansökan om bygglov</a></li>
+                            <li><a class="link-item" href="#">Ansökan till förskola</a></li>
+                            <li><a class="link-item" href="#">Felanmäl fel på gator, torg och parker</a></li>
+                            <li><a class="link-item" href="#">Tider för studentutspringet 2015</a></li>
+                            <li><a class="link-item" href="#">Barnets tider/schema på fritids</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="columns large-6">
+                <div class="box">
+                    <h3><i class="fa fa-phone"></i> Helsingborgs Kontaktcenter</h3>
+                    <div class="box-content">
+                        <ul class="list">
+                            <li>
+                                <label>Telefonnummer</label>
+                                <span class="text-lg">042-10 50 00</span>
+                            </li>
+                            <li>
+                                <label>E-postadress</label>
+                                kontaktcenter@helsingborg.se
+                            </li>
+                            <li>
+                                <label>Öppettider</label>
+                                Mån–tor 07:00–19:00, Fre 07:00–17:00, Lör 10:00–15:00
+                            </li>
+                            <li>
+                                <label>Besöksadress</label>
+                                Stortorget 17, Helsingborg
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="section-news">
+    <div class="container">
+
+        <div class="row">
+            <div class="columns large-12">
+                <h2 class="text-magenta"><i class="fa fa-newspaper-o"></i> Aktuellt i Helsingborg</h2>
+            </div>
+
+            <div class="index" data-equalizer>
+                <div class="columns large-4 medium-6">
+                    <a href="#" class="index-item" data-equalizer-watch>
+                        <img src="http://www.helsingborg.se/wp-content/uploads/2014/12/nsr_avfallskarl_300x200_foto_NSR_Rickard_Johansson_Studio-E.jpg">
+                        <span class="index-caption">Ändrade dagar för sophämtning i midsommar</span>
+                    </a>
+                </div>
+                <div class="columns large-4 medium-6">
+                    <a href="#" class="index-item" data-equalizer-watch>
+                        <img src="http://www.helsingborg.se/wp-content/uploads/2015/01/sommarlov-nyhet.jpg">
+                        <span class="index-caption">Sommarlovsprogrammet är här</span>
+                    </a>
+                </div>
+                <div class="columns large-4 medium-6 end">
+                    <a href="#" class="index-item" data-equalizer-watch>
+                        <img src="http://www.helsingborg.se/wp-content/uploads/2015/06/kommunfullmaktige_300.jpg">
+                        <span class="index-caption">Kommunfullmäktige beslutar om budgeten</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="columns large-6 medium-6">
+                <div class="box box-outlined">
+                    <h3>Nyheter</h3>
+                    <div class="box-content">
+                        <ul class="list list-links">
+                            <li><a href="#" class="link-item">Välavägen avstängd från 15 juni</a></li>
+                            <li><a href="#" class="link-item">Trafiken i korsning på Malmöleden påverkas från måndag 15 juni</a></li>
+                            <li><a href="#" class="link-item">Snart har du tidningen Vår stad i din brevlåda</a></li>
+                            <li><a href="#" class="link-item">Upplev Helsingborgs vattenliv i sommar</a></li>
+                            <li><a href="#" class="link-item">Busslinjer får nya körvägar när sommartidtabellen börjar gälla</a></li>
+                            <li><a href="#" class="link-item">Följ med på båttur med Sabella och titta på Öresunds bottendjur</a></li>
+                            <li><a href="#" class="link-item">Energisnålt byggande kan premieras</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="columns large-6 medium-6">
+                <div class="box box-outlined">
+                    <h3>Evenemang</h3>
+                    <div class="box-content">
+                        <ul class="list list-events">
+                            <li>
+                                <a href="#" class="event-item featured">
+                                    <div class="columns large-4 medium-4 small-12 featured-image">
+                                        <img src="http://www.fredriksdal.se/ImageVaultFiles/id_54706/cf_127/480x270-fredriksdal140_RalfEkvall.PNG" class="responsive">
+                                    </div>
+                                    <div class="columns large-8 medium-8 small-12">
+                                        Välkommen till Helsingborgs officiella nationaldagsfirande på Fredriksdal den 6 juni.
+                                        <span class="link-item link-item-light">Läs mer och se programmet här</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="event-item">
+                                    <span class="date">
+                                        <span class="date-day"><strong>Idag</strong></span>
+                                        <span class="date-time">10:00</span>
+                                    </span>
+                                    <span class="title link-item">Sommarteater i Det Gröna</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="event-item">
+                                    <span class="date">
+                                        <span class="date-day"><strong>Idag</strong></span>
+                                        <span class="date-time">11:00</span>
+                                    </span>
+                                    <span class="title link-item">Årets utställningar på Råå museum</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="event-item">
+                                    <span class="date">
+                                        <span class="date-day"><strong>Idag</strong></span>
+                                        <span class="date-time">12:00</span>
+                                    </span>
+                                    <span class="title link-item">"Bomber o Granater" - en gästutställning med Tintin och hans spännande värld på Toy World</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="event-item">
+                                    <span class="date">
+                                        <span class="date-day"><strong>Idag</strong></span>
+                                        <span class="date-time">14:00</span>
+                                    </span>
+                                    <span class="title link-item">Sommarteater i Det Gröna</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="event-item">
+                                    <span class="date">
+                                        <span class="date-day">17</span>
+                                        <span class="date-month">juni</span>
+                                    </span>
+                                    <span class="title link-item">Sommarteater i Det Gröna</span>
+                                </a>
+                            </li>
+                            <li><a href="#" class="list-more">Fler evenemang</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
