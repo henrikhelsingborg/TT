7a0413d59a13ae9bb9ce4381e5f64022e97bd92e
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-06-24 14:02:01 +0200

    Filter to remove empty tags in post content

--- wp-content/themes/This-is-Helsingborg/library/helpers.php
+++ wp-content/themes/This-is-Helsingborg/library/helpers.php
@@ -12,7 +12,7 @@ function get_included_pages($post) {
         'post_parent' => get_option('page_on_front'),
     );
 
-    $base_pages = get_children( $args );
+    $base_pages = get_children($args);
     foreach($base_pages as $page) {
         array_push($includes, $page->ID);
     }
@@ -40,3 +40,29 @@ function get_included_pages($post) {
 
     return implode(',', $includes);
 }
+
+/**
+ * Filters empty tags from post content
+ * @param  string $content The post content
+ * @return string          Filtered post content
+ */
+function remove_empty_p($content) {
+    // clean up p tags around block elements
+    $content = preg_replace( array(
+        '#<p>\s*<(div|aside|section|article|header|footer)#',
+        '#</(div|aside|section|article|header|footer)>\s*</p>#',
+        '#</(div|aside|section|article|header|footer)>\s*<br ?/?>#',
+        '#<(div|aside|section|article|header|footer)(.*?)>\s*</p>#',
+        '#<p>\s*</(div|aside|section|article|header|footer)#',
+        '#<p>(\s|&nbsp;)*+(<br\s*/*>)?(\s|&nbsp;)*</p>#'
+    ), array(
+        '<$1',
+        '</$1>',
+        '</$1>',
+        '<$1$2>',
+        '</$1',
+    ), $content );
+
+    return preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)*(\s|&nbsp;)*</p>#i', '', $content);
+}
+add_filter('the_content', 'remove_empty_p', 20, 1);
