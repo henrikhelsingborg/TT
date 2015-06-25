1ddc9fc1209ba60048069ca5832b334a729fae5a
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-06-25 10:50:53 +0200

    Pdf icons before pdf in search result

--- wp-content/themes/This-is-Helsingborg/assets/js/src/search/search.js
+++ wp-content/themes/This-is-Helsingborg/assets/js/src/search/search.js
@@ -43,13 +43,18 @@ jQuery(document).ready(function ($) {
             /* Get a date */
             var dateMod = null;
             if (meta.moddate !== undefined) {
-                dateMod = meta.moddate;
+                dateMod = convertDate(meta.moddate);
             } else if (meta.pubdate !== undefined) {
-                dateMod = meta.pubdate;
+                dateMod = convertDate(meta.pubdate);
             }
 
             /* Create dom for information */
-            $item.find('.search-result-item-content').append('<h3><a href="' + item.link + '" class="link-item">' + item.htmlTitle + '</a></h3>');
+            if (item.fileFormat == 'PDF/Adobe Acrobat') {
+                $item.find('.search-result-item-content').append('<h3><a href="' + item.link + '" class="pdf-item">' + item.htmlTitle + '</a></h3>');
+            } else {
+                $item.find('.search-result-item-content').append('<h3><a href="' + item.link + '" class="link-item">' + item.htmlTitle + '</a></h3>');
+            }
+
             $item.find('.search-result-item-content').append('<p>' + $.trim(item.htmlSnippet) + '</p>');
             $item.find('.search-result-item-content').append('<div class="search-result-item-info"></div>');
             $item.find('.search-result-item-info').append('<span class="search-result-item-url"><i class="fa fa-globe"></i> <a href="' + item.link + '">' + item.htmlFormattedUrl + '</a></span>');
@@ -59,6 +64,57 @@ jQuery(document).ready(function ($) {
             /* Append the item to the result container */
             $resultContainer.append($item);
         });
+    }
+
+    function convertDate(value) {
+        if (value.length > 20) {
+            var year = value.substring(2,6);
+            var month = value.substring(6,8);
+            var day = value.substring(8,10);
+            month = convertDateToMonth(month);
+            return day + ' ' + month + ' ' + year;
+        } else if (value.length == 11) {
+            value = value.replace('May', 'Maj');
+            value = value.replace('Oct', 'Okt');
+            return value;
+        } else if (value.length == 8) {
+            var year = value.substring(0,4);
+            var month = value.substring(4,6);
+            var day = value.substring(6,value.length);
+            month = convertDateToMonth(month);
+            return day + ' ' + month + ' ' + year;
+        } else {
+            return '';
+        }
+    }
+
+    function convertDateToMonth(month) {
+        switch (month) {
+            case '01':
+                return "Jan";
+            case '02':
+                return "Feb";
+            case '03':
+                return "Mar";
+            case '04':
+                return "Apr";
+            case '05':
+                return "Maj";
+            case '06':
+                return "Jun";
+            case '07':
+                return "Jul";
+            case '08':
+                return "Aug";
+            case '09':
+                return "Sep";
+            case '10':
+                return "Okt";
+            case '11':
+                return "Nov";
+            case '12':
+                return "Dec";
+        }
     }
 
 });
