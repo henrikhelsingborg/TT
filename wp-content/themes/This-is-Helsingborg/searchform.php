ee31d7eeb45d7b6efff0106b754aa25a1b257207
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-06-25 09:46:11 +0200

    Searchform UI fixes

--- wp-content/themes/This-is-Helsingborg/searchform.php
+++ wp-content/themes/This-is-Helsingborg/searchform.php
@@ -1,12 +1,13 @@
-<form class="<?php echo (is_front_page()) ? 'hero-search center-vertical' : 'sidebar-search'; ?>" method="get" role="search" action="/">
-    <?php echo (is_front_page()) ? '<div class="inline-block">' : ''; ?>
-    <div class="input-group">
-        <div class="form-element">
-            <input class="form-control" type="search" name="s" placeholder="Här kan du skriva vad du letar efter…" value="<?php echo (isset($_GET['s']) && strlen($_GET['s']) > 0) ? $_GET['s'] : ''; ?>">
-        </div>
-        <div class="form-element">
-            <button class="form-control btn btn-submit" type="submit"><?php echo (is_front_page()) ? '<i class="fa fa-search"></i>' : 'Sök'; ?></button>
+<form class="search" method="get" role="search" action="/">
+    <div class="form-container">
+        <h2><strong>Hej,</strong> vad letar du efter?</h2>
+        <div class="input-group">
+            <div class="form-element">
+                <input class="form-control" type="search" name="s" placeholder="Här kan du skriva vad du letar efter…" value="<?php echo (isset($_GET['s']) && strlen($_GET['s']) > 0) ? $_GET['s'] : ''; ?>">
+            </div>
+            <div class="form-element">
+                <button class="form-control btn btn-submit" type="submit"><i class="fa fa-search"></i>Sök</button>
+            </div>
         </div>
     </div>
-    <?php echo (is_front_page()) ? '</div>' : ''; ?>
 </form>
--- wp-content/themes/This-is-Helsingborg/templates/partials/sidebar-left.php
+++ wp-content/themes/This-is-Helsingborg/templates/partials/sidebar-left.php
@@ -1,4 +1,4 @@
-<aside class="columns large-3 medium-3 hide-for-small-only">
+<aside class="sidebar-left columns large-3 medium-3 hide-for-small-only">
     <?php
         get_template_part('templates/partials/sidebar', 'search');
         get_template_part('templates/partials/sidebar', 'navigation');
