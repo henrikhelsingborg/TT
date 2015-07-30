d77349bfb3626ac35735dbdddec7ca725221f23c
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-07-30 15:58:04 +0200

    Improved share ui

--- wp-content/themes/This-is-Helsingborg/templates/partials/article-share.php
+++ wp-content/themes/This-is-Helsingborg/templates/partials/article-share.php
@@ -16,9 +16,9 @@
             <h5><span>Dela sidan:</span> <?php the_title(); ?></h5>
             <span class="share-url"><?php echo wp_get_shortlink(); ?></span>
             <ul class="share-icons">
-                <li><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_the_permalink()); ?>" title="Dela på Facebook"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="http://twitter.com/share?url=<?php echo urlencode(wp_get_shortlink()); ?>" title="Dela på Twitter"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo urlencode(get_the_permalink()); ?>&amp;title=<?php echo get_the_title(); ?>&amp;summary=<?php echo get_the_excerpt(); ?>&amp;source=Helsingborg.se" title="Dela på LinkedIn"><i class="fa fa-linkedin"></i></a></li>
+                <li><a data-action="share-popup" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_the_permalink()); ?>" title="Dela på Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a data-action="share-popup" href="http://twitter.com/share?url=<?php echo urlencode(wp_get_shortlink()); ?>" title="Dela på Twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a data-action="share-popup" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo urlencode(get_the_permalink()); ?>&amp;title=<?php echo get_the_title(); ?>&amp;summary=<?php echo get_the_excerpt(); ?>&amp;source=Helsingborg.se" title="Dela på LinkedIn"><i class="fa fa-linkedin"></i></a></li>
             </ul>
         </div>
         <div class="clearfix"></div>
