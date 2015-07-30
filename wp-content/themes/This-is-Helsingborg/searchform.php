862cfb680333f227b269333b4179407724b3014f
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-07-30 16:16:51 +0200

    Search form adjustment

--- wp-content/themes/This-is-Helsingborg/searchform.php
+++ wp-content/themes/This-is-Helsingborg/searchform.php
@@ -3,7 +3,7 @@
         <h2><strong>Hej,</strong> vad letar du efter?</h2>
         <div class="input-group">
             <div class="form-element">
-                <input class="form-control" type="search" name="s" placeholder="Här kan du skriva vad du letar efter…" value="<?php echo (isset($_GET['s']) && strlen($_GET['s']) > 0) ? $_GET['s'] : ''; ?>">
+                <input class="form-control" type="search" name="s" placeholder="<?php echo (is_front_page()) ? 'Här kan du skriva vad du letar efter…' : 'Vad letar du efter?'; ?>" value="<?php echo (isset($_GET['s']) && strlen($_GET['s']) > 0) ? $_GET['s'] : ''; ?>">
             </div>
             <div class="form-element">
                 <button class="form-control btn btn-submit" type="submit"><i class="fa fa-search"></i>Sök</button>
