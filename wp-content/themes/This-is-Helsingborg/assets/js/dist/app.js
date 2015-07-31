61f5a1f30dbf9dded0819d4386e76510a70842b6
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-07-31 14:47:21 +0200

    Added vergic script

--- wp-content/themes/This-is-Helsingborg/assets/js/dist/app.js
+++ wp-content/themes/This-is-Helsingborg/assets/js/dist/app.js
@@ -282,6 +282,12 @@ Helsingborg.Share.Button = (function ($) {
     return new Button();
 
 })(jQuery);
+(function (server, psID) {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = server + '/' + psID + '/ps.js';
+    document.getElementsByTagName('head')[0].appendChild(s);
+}('https://account.psplugin.com', '331F5271-4B0B-4625-BF08-4157F101DBFF'));
 /*
  * Foundation Responsive Library
  * http://foundation.zurb.com
