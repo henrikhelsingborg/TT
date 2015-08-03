79764066a2ca3408b017233c670e263d3247a0ad
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-08-03 10:28:13 +0200

    Cookie acceptance message first time visiting the site (or if no value is set)

--- wp-content/themes/This-is-Helsingborg/header.php
+++ wp-content/themes/This-is-Helsingborg/header.php
@@ -71,6 +71,8 @@
             ?>
         </div>
 
+        <div data-prompt-wrapper="alert"></div>
+
         <?php
             if (is_front_page()) {
                 get_template_part('templates/partials/hero');
