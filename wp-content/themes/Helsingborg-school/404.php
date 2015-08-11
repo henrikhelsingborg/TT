b9156ec4e76e7c53a785f48842e3e10d284be66b
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-08-11 11:19:58 +0200

    Searchform on 404 page

--- wp-content/themes/Helsingborg-school/404.php
+++ wp-content/themes/Helsingborg-school/404.php
@@ -5,7 +5,7 @@
         <div class="main-content columns large-12">
             <h1>Sidan kan inte hittas</h1>
             <?php get_search_form(); ?>
-            <p><?php _e('Den sida du vill nå kan inte hittas. Du kan använda sökfunktionen för att försöka hitta det du letar efter.', 'Helsingborg'); ?></p>
+            <p style="margin-top:20px;"><?php _e('Den sida du vill nå kan inte hittas. Du kan använda sökfunktionen för att försöka hitta det du letar efter.', 'Helsingborg'); ?></p>
             <p><?php _e('<h3>Det här kan du göra för att hitta det du söker:</h3>', 'Helsingborg'); ?></p>
             <ul>
                 <li><?php _e('Kontrollera stavningen. Har du skrivit rätt adress?', 'Helsingborg'); ?></li>
---
+++ wp-content/themes/Helsingborg-school/searchform.php
@@ -0,0 +1,16 @@
+<form class="search" method="get" role="search" action="/">
+    <div class="form-container">
+        <div class="input-group">
+            <div class="form-element">
+                <input <?php if (is_front_page()) echo 'data-autocomplete="pages"'; ?> autocomplete="off" class="form-control" type="search" name="s" placeholder="<?php echo (is_front_page()) ? 'Här kan du skriva vad du letar efter…' : 'Vad letar du efter?'; ?>" value="<?php echo (isset($_GET['s']) && strlen($_GET['s']) > 0) ? urldecode($_GET['s']) : ''; ?>">
+                <?php if (is_front_page()) : ?>
+                    <div class="hbg-loading hbg-loading-sm"></div>
+                    <ul class="autocomplete"></ul>
+                <?php endif; ?>
+            </div>
+            <div class="form-element submit-action">
+                <button class="form-control btn btn-submit" type="submit"><i class="fa fa-search"></i> Sök</button>
+            </div>
+        </div>
+    </div>
+</form>
