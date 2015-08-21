6170f1b6780bda2ad9f93cea31cf09b825caa384
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-08-21 09:05:11 +0200

    Moved xcap and cbis scheduled tasks from the theme to the Helsingborg Widgets plugin

--- wp-content/plugins/helsingborg-widgets/helsingborg-event/helsingborg-event.php
+++ wp-content/plugins/helsingborg-widgets/helsingborg-event/helsingborg-event.php
@@ -4,3 +4,30 @@
     require_once('models/event_model.php');
 
     $EventList = new EventList();
+
+
+   /**
+    * Add scheduled work for CBIS events
+    * @return void
+    */
+    require_once('cron/scheduled_cbis.php');
+    function setup_scheduled_cbis() {
+      if ( ! wp_next_scheduled( 'scheduled_cbis' ) ) {
+        // Set scheduled task to occur at 22.30 each day
+        wp_schedule_event( strtotime(date("Y-m-d", time()) . '22:30'), 'daily', 'scheduled_cbis');
+      }
+    }
+    add_action('wp', 'setup_scheduled_cbis');
+
+    /**
+     * Add scheduled work for XCap events
+     * @return void
+     */
+    require_once('cron/scheduled_xcap.php');
+    function setup_scheduled_xcap() {
+      if ( ! wp_next_scheduled( 'scheduled_xcap' ) ) {
+        // Set scheduled task to occur at 22.30 each day
+        wp_schedule_event( strtotime(date("Y-m-d", time()) . '22:30'), 'daily', 'scheduled_xcap');
+      }
+    }
+    add_action('wp', 'setup_scheduled_xcap');
--- wp-content/themes/This-is-Helsingborg/lib/Public.php
+++ wp-content/themes/This-is-Helsingborg/lib/Public.php
@@ -25,3 +25,18 @@ function get_excerpt_by_id($postId = 0) {
 function get_included_pages($post) {
     Helsingborg\Helper\Wp::getIncludedPages($post);
 }
+
+/**
+ * Cron interval used by our schedule events
+ * @param  [type] $schedules [description]
+ * @return [type]            [description]
+ */
+function cron_add_3min($schedules) {
+    $schedules['3min'] = array(
+        'interval' => 3*60,
+        'display'  => __('Once every three minutes')
+    );
+
+    return $schedules;
+}
+add_filter('cron_schedules', 'cron_add_3min');
