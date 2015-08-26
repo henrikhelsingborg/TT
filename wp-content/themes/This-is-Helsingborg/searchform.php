364430f8b6d1ab79e0c1f3774e71cdeb4070665b
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-08-26 13:39:38 +0200

    Search headline is now a label instead of a h2

--- wp-content/themes/This-is-Helsingborg/searchform.php
+++ wp-content/themes/This-is-Helsingborg/searchform.php
@@ -1,6 +1,6 @@
 <form class="search" method="get" role="search" action="/">
     <div class="form-container">
-        <h2><strong>Hej,</strong> vad letar du efter?</h2>
+        <label class="search-label"><strong>Hej,</strong> vad letar du efter?</label>
         <div class="input-group">
             <div class="form-element">
                 <input <?php if (is_front_page()) echo 'data-autocomplete="pages"'; ?> autocomplete="off" class="form-control" type="search" name="s" placeholder="<?php echo (is_front_page()) ? 'Här kan du skriva vad du letar efter…' : 'Vad letar du efter?'; ?>" value="<?php echo (isset($_GET['s']) && strlen($_GET['s']) > 0) ? urldecode($_GET['s']) : ''; ?>">
