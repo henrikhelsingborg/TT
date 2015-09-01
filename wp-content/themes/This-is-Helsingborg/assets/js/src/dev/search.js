0cca9d580ad57609faa1e4458ec3b2ad411a8ffc
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-09-01 11:16:53 +0200

    #1874 - Better loading indicator in search button

--- wp-content/themes/This-is-Helsingborg/assets/js/src/dev/search.js
+++ wp-content/themes/This-is-Helsingborg/assets/js/src/dev/search.js
@@ -202,7 +202,7 @@ Helsingborg.Search.Button = (function ($) {
     Button.prototype.handleEvents = function() {
 
         $(document).on('click', '.search .btn-submit', function (e) {
-            if ($(this).parents('.hero').length) {
+            if ($(this).parents('.hero').length || $(this).parents('.site-header').length) {
                 $(this).html('<i class="dots-loading dots-loading-small"></i>');
             } else {
                 $(this).html('<i class="dots-loading"></i>');
