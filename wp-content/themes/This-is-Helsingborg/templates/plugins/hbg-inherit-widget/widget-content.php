ae862067d6b09a170cc238d97fee6fbf70d498fd
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-09-03 08:31:28 +0200

    #1876 - Inherit widget template for slider area

--- wp-content/themes/This-is-Helsingborg/templates/plugins/hbg-inherit-widget/widget-content.php
+++ wp-content/themes/This-is-Helsingborg/templates/plugins/hbg-inherit-widget/widget-content.php
@@ -1,14 +1,30 @@
-<?php echo $before_widget; ?>
-<div class="widget-content">
-    <div class="widget-content-holder">
-        <h2><?php echo $post->post_title; ?></h2>
-        <div class="divider">
-            <div class="upper-divider"></div>
-            <div class="lower-divider"></div>
-        </div>
-        <div class="textwidget">
-            <?php echo the_content(); ?>
+<?php
+echo $before_widget;
+if ($args['id'] == 'slider-area') :
+?>
+
+    <h3 class="widget-title"><?php echo $post->post_title; ?></h3>
+
+    <div class="box-content box-content-padding-x2">
+        <?php echo the_content(); ?>
+    </div>
+
+<?php else : ?>
+
+    <div class="widget-content">
+        <div class="widget-content-holder">
+            <h2><?php echo $post->post_title; ?></h2>
+            <div class="divider">
+                <div class="upper-divider"></div>
+                <div class="lower-divider"></div>
+            </div>
+            <div class="textwidget">
+                <?php echo the_content(); ?>
+            </div>
         </div>
     </div>
-</div>
-<?php echo $after_widget; ?>
+
+<?php
+endif;
+echo $after_widget;
+?>
