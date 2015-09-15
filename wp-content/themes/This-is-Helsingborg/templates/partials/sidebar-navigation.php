566502a5b8ccdc51a1c741a93aefa16d0e210f03
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-09-15 12:51:25 +0200

    #1892 - Do not show empty left sidebar menu if no pages exist

--- wp-content/themes/This-is-Helsingborg/templates/partials/sidebar-navigation.php
+++ wp-content/themes/This-is-Helsingborg/templates/partials/sidebar-navigation.php
@@ -2,23 +2,24 @@
     /* Generates the page tree, skips drafted pages */
     require_once(get_template_directory() . '/lib/Walker/helsingborg-walker.php');
     $walker_page = new Helsingborg_Walker();
+
+    $menu = wp_cache_get('menu_' . $post->ID);
+    if ( false === $menu ) {
+        $menu = wp_list_pages(array(
+            'title_li' => '',
+            'echo'     => 0,
+            'walker'   => $walker_page,
+            'include'  => get_included_pages($post)
+        ));
+
+        wp_cache_set('menu_' . $post->ID , $menu);
+    }
+
+    if ($menu) :
 ?>
 <nav class="navbar-aside">
     <ul class="nav nav-list">
-        <?php
-            $menu = wp_cache_get('menu_' . $post->ID);
-            if ( false === $menu ) {
-                $menu = wp_list_pages(array(
-                    'title_li' => '',
-                    'echo'     => 0,
-                    'walker'   => $walker_page,
-                    'include'  => get_included_pages($post)
-                ));
-
-                wp_cache_set('menu_' . $post->ID , $menu);
-            }
-
-            echo $menu;
-        ?>
+        <?php echo $menu; ?>
     </ul>
 </nav>
+<?php endif; ?>
