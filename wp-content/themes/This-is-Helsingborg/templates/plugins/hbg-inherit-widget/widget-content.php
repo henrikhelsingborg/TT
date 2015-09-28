945ce82dd929ddcf526ee1913b383884fcfa357a
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-09-28 14:27:29 +0200

    Removed unnecessary template files

--- wp-content/themes/This-is-Helsingborg/templates/plugins/hbg-inherit-widget/widget-content.php
+++ wp-content/themes/This-is-Helsingborg/templates/plugins/hbg-inherit-widget/widget-content.php
@@ -1,3 +1,7 @@
-<?php
+<?php echo $before_widget; ?>
+    <h3 class="box-title"><?php echo $post->post_title; ?></h3>
 
-    include('widget-default.php');
+    <div class="box-content padding-x2">
+        <?php echo the_content(); ?>
+    </div>
+<?php echo $after_widget; ?>
