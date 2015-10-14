5b073deef2bf51fcde68cd926d8241d12b36cbb1
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-10-14 08:08:54 +0200

    Event form bug fixed

--- wp-content/plugins/helsingborg-widgets/helsingborg-event/helsingborg-event.php
+++ wp-content/plugins/helsingborg-widgets/helsingborg-event/helsingborg-event.php
@@ -1,6 +1,7 @@
 <?php
 
     include_once('classes/helsingborg-event-list-widget.php');
+    require_once('happy-event.php');
     require_once('models/event_model.php');
 
     $EventList = new EventList();
