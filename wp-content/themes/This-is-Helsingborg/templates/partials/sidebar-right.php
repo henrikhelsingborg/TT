fa5fc79c1125d59160de14db2eef1b8635c2b425
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-10-14 14:13:31 +0200

    #1982 - Hide the sidebar left menu when the mobile menu is displayed

--- wp-content/themes/This-is-Helsingborg/templates/partials/sidebar-right.php
+++ wp-content/themes/This-is-Helsingborg/templates/partials/sidebar-right.php
@@ -1,9 +1,5 @@
 <aside class="sidebar-right columns large-3 medium-12 small-12 print-hidden">
     <div class="row">
-        <div class="columns large-12 medium-8 small-12 right">
-            <div class="row">
-                <?php dynamic_sidebar('right-sidebar'); ?>
-            </div>
-        </div>
+        <?php dynamic_sidebar('right-sidebar'); ?>
     </div>
 </aside>
