fafa1a1a820ff95796b014355684c6cdf6630128
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-10-20 15:39:52 +0200

    Revert "#2001 - Remove/hide the "post" post type since it's not used"
    This reverts commit cdac83c129409517b3e946be094d63ebd41b6244.

--- wp-content/themes/This-is-Helsingborg/lib/Theme/Support.php
+++ wp-content/themes/This-is-Helsingborg/lib/Theme/Support.php
@@ -13,35 +13,12 @@ class Support
 
         add_filter('srm_max_redirects', array($this, 'srmMaxRedirects'));
         add_action('template_redirect', array($this, 'blockAuthorPages'), 5);
-        add_action('init', array($this, 'removePostPostType'), 11);
-    }
-
-    /**
-     * Removes the post type "post"
-     * @return boolean
-     */
-    public function removePostPostType()
-    {
-        global $wp_post_types;
-
-        if (isset($wp_post_types['post'])) {
-            if (!defined("WP_ENABLE_POSTS") || (defined("WP_ENABLE_POSTS") && WP_ENABLE_POSTS !== true)) {
-                unset($wp_post_types['post']);
-                add_action('admin_menu', function () {
-                    remove_menu_page('edit.php');
-                });
-            }
-
-            return true;
-        }
-
-        return false;
     }
 
     /**
      * Removes unwanted actions.
      */
-    private static function removeActions()
+    static private function removeActions()
     {
         remove_action('wp_head', 'print_emoji_detection_script', 7);
         remove_action('wp_print_styles', 'print_emoji_styles');
@@ -52,7 +29,7 @@ class Support
     /**
      * Add actions.
      */
-    private static function addActions()
+    static private function addActions()
     {
         add_action('after_setup_theme', '\Helsingborg\Theme\Support::themeSupport');
     }
@@ -60,7 +37,7 @@ class Support
     /**
      * Add filters.
      */
-    private static function addFilters()
+    static private function addFilters()
     {
         add_filter('intermediate_image_sizes_advanced', '\Helsingborg\Theme\Support::filterThumbnailSizes');
         add_filter('gettext', '\Helsingborg\Theme\Support::changeDefaultTemplateName', 10, 3);
@@ -73,19 +50,16 @@ class Support
     {
         add_theme_support('menus');
         add_theme_support('post-thumbnails');
-        add_theme_support(
-            'post-formats',
-            array(
-                'aside',
-                'gallery',
-                'link',
-                'image',
-                'quote',
-                'status',
-                'video',
-                'audio',
-                'chat'
-            )
+        add_theme_support('post-formats', array(
+            'aside',
+            'gallery',
+            'link',
+            'image',
+            'quote',
+            'status',
+            'video',
+            'audio',
+            'chat')
         );
     }
 
@@ -125,8 +99,7 @@ class Support
      * Blocks request to the author pages (?author=<ID>).
      * @return void
      */
-    public function blockAuthorPages()
-    {
+    public function blockAuthorPages() {
         global $wp_query;
 
         if (is_author() || is_attachment()) {
