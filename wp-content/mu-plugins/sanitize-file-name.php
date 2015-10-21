5a0232f602162fea1ffe66a51491236c6a3a7693
Sebastian Thulin <sebastian.thulin@gmail.com>
2015-10-21 14:19:17 +0200

    #2022 - Santitizing file-names with wp_handle_upload_prefilter and santize_title

---
+++ wp-content/mu-plugins/sanitize-file-name.php
@@ -0,0 +1,18 @@
+<?php
+	
+	/*
+	Plugin Name: Helsingborg santitize file names (Must use plugin)
+	Description: Remove special carachers from uploaded files. 
+	Version:     1.0 
+	Author:      Sebastian Thulin @ Helsingborgs Stad 
+	*/
+	
+	add_action('wp_handle_upload_prefilter', function($file) {
+		
+		$path 			= pathinfo($file['name']);
+		$new_filename 	= preg_replace('/.' . $path['extension'] . '$/', '', $file['name']);
+		$file['name'] 	= sanitize_title($new_filename) . '.' . $path['extension'];
+
+		return $file;
+		
+	}); 
