c35e9614119ab20d136fadf0a6f69b74618e17ac
Sebastian Thulin <sebastian.thulin@gmail.com>
2015-10-26 11:26:21 +0100

    Updated meta_boxes function for IIS servers

--- wp-content/themes/This-is-Helsingborg/meta_boxes/UI/list-array.php
+++ wp-content/themes/This-is-Helsingborg/meta_boxes/UI/list-array.php
@@ -1,11 +1,33 @@
 <?php
+	
+	//TODO: Remove this code. 
+	
+	if ( !function_exists('hbg_meta_box_ui') ) {
+		
+		function hbg_meta_box_ui () {
+			
+			// Get list categories from db table
+			global $wpdb;
+			
+			try {
+			    $listResult = $wpdb->get_results("SELECT title FROM list_categories ORDER BY id ASC", OBJECT);
+			} catch (Exception $e) {
+				$listResult = false; 
+			}
 
-// Get list categories from db table
-global $wpdb;
-$listResult = $wpdb->get_results("SELECT title FROM list_categories ORDER BY id ASC", OBJECT);
-
-// Setup list categories as an array formatted array(0 => 'TITLE');
-$list = array();
-foreach ($listResult as $item) {
-    $list[] = $item->title;
-}
+			// Setup list categories as an array formatted array(0 => 'TITLE');
+			$list = array();
+			
+			if ( !empty( $listResult ) ) { 
+				
+				foreach ($listResult as $item) {
+				    $list[] = $item->title;
+				}
+				
+			}
+			
+			return apply_filters('filter_hbg_meta_box_ui', $list); 
+		}
+	} 
+	
+	$list = hbg_meta_box_ui(); 
