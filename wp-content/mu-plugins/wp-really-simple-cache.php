700b232eb3fa1319bbc5eb9d89016a05e9068597
Sebastian Thulin <sebastian.thulin@gmail.com>
2015-11-02 09:46:20 +0100

    Add functions exists omn cache plugin

--- wp-content/mu-plugins/wp-really-simple-cache.php
+++ wp-content/mu-plugins/wp-really-simple-cache.php
@@ -10,183 +10,189 @@ Author:      Sebastian Thulin @ Helsingborg Stad
 */
 
 /* Store callback */ //TODO: FIX THIS
-function wp_simple_cache_plugin_end ( $data ) {
-
-	//Cache data
-	$cache_instance = new WpSimpleCache();
-	$cache_instance::store_cache($data);
-
-	//Return to output
-	return $data;
-
+if ( !function_exists( 'wp_simple_cache_plugin_end' ) ) { 
+	function wp_simple_cache_plugin_end ( $data ) {
+	
+		//Cache data
+		$cache_instance = new WpSimpleCache();
+		$cache_instance::store_cache($data);
+	
+		//Return to output
+		return $data;
+	
+	}
 }
 
 global $wp_simple_cache;
 
-Class WpSimpleCache {
+if ( !class_exists( 'WpSimpleCache' ) ) { 
 
-	private static $file_hash;
-	private static $domain_name;
-	private static $cache_time;
-	private static $cache_folder;
-
-	private static $blocked_urls;
-
-	public function __construct() {
-
-		//Setup variables
-		self::$file_hash 		= md5(rtrim(trim($_SERVER['REQUEST_URI']),"/"));
-		self::$domain_name 		= md5($_SERVER['SERVER_NAME']);
-		self::$cache_time 		= 60 * 60 * 60 * 24 * 7; //In seconds
-		self::$cache_folder		= "/cache/";
-
-		//What urls should not be cached?
-		self::$blocked_urls		= array("wp-admin","wp-login","secure");
-
-		//Setup
-		self::setup_folders();
-
-	}
-
-	public function init() {
-		//Cache logic
-		if (self::is_cachable()) {
-			self::start();
+	Class WpSimpleCache {
+	
+		private static $file_hash;
+		private static $domain_name;
+		private static $cache_time;
+		private static $cache_folder;
+	
+		private static $blocked_urls;
+	
+		public function __construct() {
+	
+			//Setup variables
+			self::$file_hash 		= md5(rtrim(trim($_SERVER['REQUEST_URI']),"/"));
+			self::$domain_name 		= md5($_SERVER['SERVER_NAME']);
+			self::$cache_time 		= 60 * 60 * 60 * 24 * 7; //In seconds
+			self::$cache_folder		= "/cache/";
+	
+			//What urls should not be cached?
+			self::$blocked_urls		= array("wp-admin","wp-login","secure");
+	
+			//Setup
+			self::setup_folders();
+	
 		}
-	}
-
-	private static function get_filename () {
-		return self::get_cache_dir().self::$file_hash.".html.gz";
-	}
-
-	public static function get_cache_dir() {
-		return self::base_dir().self::$cache_folder.self::$domain_name."/";
-	}
-
-	private static function get_cache() {
-        return readgzfile(self::get_filename());
-    }
-
-    public static function setup_folders () {
-	    if ( !is_dir( self::base_dir().self::$cache_folder  ) ) {
-		    mkdir( self::base_dir().self::$cache_folder , 0775, true);
-	    }
-	    if ( !is_dir( self::base_dir().self::$cache_folder.self::$domain_name."/" ) ) {
-		    mkdir( self::base_dir().self::$cache_folder.self::$domain_name."/" , 0775, true);
-	    }
-    }
-
-    private static function base_dir() {
-	    if ( defined('WP_SIMPLE_CACHE_BASE_DIR') ) {
-		    return rtrim( WP_SIMPLE_CACHE_BASE_DIR, "/");
-	    } else {
-		    return __DIR__;
-	    }
-    }
-
-    public static function start() {
-
-	    //Check if cache exists
-	    if (file_exists(self::get_filename()) && (time() - self::$cache_time < filemtime(self::get_filename()))) {
-	    	self::get_cache();
-	    	exit;
-	    } else {
-			ob_start('WpSimpleCachePlugin\Cache\wp_simple_cache_plugin_end');
-	    }
-    }
-
-	public static function store_cache($callback_data) {
-
-		//Go back to current dir (applys to some apache servers)
-		chdir(dirname($_SERVER['SCRIPT_FILENAME']));
-
-		//Check if cache is valid (empty pages should not be cached)
-		if ( !empty( $callback_data ) ) {
-
-			//Create handle
-			$file_handle = fopen(self::get_filename(),"wa+");
-
-			//Write new cache
-			fwrite($file_handle, gzencode($callback_data, 9));
-			
-			//Set correct user rights
-			chmod(self::get_filename(), 0775);
-
+	
+		public function init() {
+			//Cache logic
+			if (self::is_cachable()) {
+				self::start();
+			}
 		}
-
-		//Return string according to ob_cache documentation
-		return $callback_data;
-
-	}
-
-	//Clean cache
-	public static function clean_cache() {
-	    $files = glob(self::get_cache_dir()."*");
-	    if ( !empty( $files ) && is_array( $files ) ) {
-		    foreach($files as $file) {
-		    	if(is_file($file)) {
-				    unlink($file);
-		    	}
+	
+		private static function get_filename () {
+			return self::get_cache_dir().self::$file_hash.".html.gz";
+		}
+	
+		public static function get_cache_dir() {
+			return self::base_dir().self::$cache_folder.self::$domain_name."/";
+		}
+	
+		private static function get_cache() {
+	        return readgzfile(self::get_filename());
+	    }
+	
+	    public static function setup_folders () {
+		    if ( !is_dir( self::base_dir().self::$cache_folder  ) ) {
+			    mkdir( self::base_dir().self::$cache_folder , 0775, true);
+		    }
+		    if ( !is_dir( self::base_dir().self::$cache_folder.self::$domain_name."/" ) ) {
+			    mkdir( self::base_dir().self::$cache_folder.self::$domain_name."/" , 0775, true);
 		    }
 	    }
-    }
-
-	//BLOCK SOME URLS FROM CACHE
-
-	public static function is_blocked_url () {
-
-		if ( is_array( self::$blocked_urls ) && !empty( self::$blocked_urls ) ) {
-
-			foreach ( self::$blocked_urls as $blocked_url) {
-				if ( preg_match("/".$blocked_url."/i", isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : '' ) ) {
-					return true;
-				}
+	
+	    private static function base_dir() {
+		    if ( defined('WP_SIMPLE_CACHE_BASE_DIR') ) {
+			    return rtrim( WP_SIMPLE_CACHE_BASE_DIR, "/");
+		    } else {
+			    return __DIR__;
+		    }
+	    }
+	
+	    public static function start() {
+	
+		    //Check if cache exists
+		    if (file_exists(self::get_filename()) && (time() - self::$cache_time < filemtime(self::get_filename()))) {
+		    	self::get_cache();
+		    	exit;
+		    } else {
+				ob_start('WpSimpleCachePlugin\Cache\wp_simple_cache_plugin_end');
+		    }
+	    }
+	
+		public static function store_cache($callback_data) {
+	
+			//Go back to current dir (applys to some apache servers)
+			chdir(dirname($_SERVER['SCRIPT_FILENAME']));
+	
+			//Check if cache is valid (empty pages should not be cached)
+			if ( !empty( $callback_data ) ) {
+	
+				//Create handle
+				$file_handle = fopen(self::get_filename(),"wa+");
+	
+				//Write new cache
+				fwrite($file_handle, gzencode($callback_data, 9));
+				
+				//Set correct user rights
+				chmod(self::get_filename(), 0775);
+	
 			}
-
+	
+			//Return string according to ob_cache documentation
+			return $callback_data;
+	
 		}
-
-		return false;
-
-	}
-
-	private static function is_post_request () {
-		if( isset( $_POST ) && !empty( $_POST ) ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	private static function has_get_variable () {
-		if( isset( $_GET ) && !empty( $_GET ) ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	private static function is_logged_in () {
-
-		if ( count( $_COOKIE ) ) {
-
-			foreach($_COOKIE as $key => $value ){
-
-			    if( preg_match("/wordpress_logged_in/i", $key ) ){
-			        return true;
+	
+		//Clean cache
+		public static function clean_cache() {
+		    $files = glob(self::get_cache_dir()."*");
+		    if ( !empty( $files ) && is_array( $files ) ) {
+			    foreach($files as $file) {
+			    	if(is_file($file)) {
+					    unlink($file);
+			    	}
 			    }
+		    }
+	    }
+	
+		//BLOCK SOME URLS FROM CACHE
+	
+		public static function is_blocked_url () {
+	
+			if ( is_array( self::$blocked_urls ) && !empty( self::$blocked_urls ) ) {
+	
+				foreach ( self::$blocked_urls as $blocked_url) {
+					if ( preg_match("/".$blocked_url."/i", isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : '' ) ) {
+						return true;
+					}
+				}
+	
 			}
-
-		}
-		return false;
-
-	}
-
-	private static function is_cachable () {
-		if ( !self::is_blocked_url() && !self::is_logged_in () && !self::is_post_request() && !self::has_get_variable () ) {
-			return true;
-		} else {
+	
 			return false;
+	
 		}
+	
+		private static function is_post_request () {
+			if( isset( $_POST ) && !empty( $_POST ) ) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	
+		private static function has_get_variable () {
+			if( isset( $_GET ) && !empty( $_GET ) ) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	
+		private static function is_logged_in () {
+	
+			if ( count( $_COOKIE ) ) {
+	
+				foreach($_COOKIE as $key => $value ){
+	
+				    if( preg_match("/wordpress_logged_in/i", $key ) ){
+				        return true;
+				    }
+				}
+	
+			}
+			return false;
+	
+		}
+	
+		private static function is_cachable () {
+			if ( !self::is_blocked_url() && !self::is_logged_in () && !self::is_post_request() && !self::has_get_variable () ) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	
 	}
 
 }
