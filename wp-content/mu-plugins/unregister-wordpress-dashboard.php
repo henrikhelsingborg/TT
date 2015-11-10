dde58697e6ccd864a7d8f1f5b0eeb53be8bccff3
Sebastian Thulin <sebastian.thulin@gmail.com>
2015-11-10 15:09:00 +0100

    #2019 - Added some dashboards functions again.
    Also removed cache plugin (no release ready)

--- wp-content/mu-plugins/unregister-wordpress-dashboard.php
+++ wp-content/mu-plugins/unregister-wordpress-dashboard.php
@@ -13,18 +13,13 @@
 		
 		// Remove wordpress dashboards 
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);
-		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
-		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
 		
 		// Remove yoast seo
 		unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']);
-		
-		// Remove gravity forms
-		unset($wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard']);
 		
 	}, 999 );
