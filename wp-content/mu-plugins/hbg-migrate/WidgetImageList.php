a1b78c5d94e8a1d5025b1b611a28b2bcc7a51ad1
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-13 15:58:00 +0100

    ImageList widget to slider module migrater

---
+++ wp-content/mu-plugins/hbg-migrate/WidgetImageList.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace HbgMigrate;
+
+class WidgetImageList extends \HbgMigrate\Widget
+{
+    public $widgetType = 'image_list_widget';
+    public $moduleType = 'mod-slider';
+
+    public function migrate(array $widgetData, int $postId)
+    {
+        $images = array();
+
+        for ($i = 1; $i < $widgetData['amount']; $i++) {
+            // Upload image
+            $imageId = $this->saveImageFromUrl($widgetData['imageurl' . $i], $postId);
+
+            // Initial slide data
+            $images[$i] = array(
+                'acf_fc_layout' => 'image',
+                'field_56a5ed2f398dc' => $imageId, // image
+                'field_570f4e9b10c26' => $imageId // mobile_image
+            );
+
+            // Text
+            if (!empty($widgetData['item_text' . $i])) {
+                $images[$i]['field_56ab224ac2c28'] = 1;
+                $images[$i]['field_56e7fa620ee0a'] = 'bottom';
+                $images[$i]['field_56b9f3b8d7720'] = $widgetData['item_text' . $i];
+            }
+
+            // Link
+            $images[$i]['field_56fa87ec3ace2'] = 'false';
+            if (!empty($widgetData['item_link' . $i])) {
+                $images[$i]['field_56fa87ec3ace2'] = 'external'; // link_type
+                $images[$i]['field_56fa87fa3ace4'] = $widgetData['item_link' . $i]; // link_url
+                $images[$i]['field_56fa880c3ace6'] = $widgetData['item_target' . $i]; // link_target
+            }
+        }
+
+        $data = array(
+            'post_title' => $widgetData['title'],
+            'post_content' => '',
+            'acf' => array(
+                'field_573dce058a66t' => 'default', // slider_layout
+                'field_573dce058a66e' => 'ratio-16-9', // slider_format
+                'field_58934110b566f' => '1', // slider_columns
+                'field_58934110b566e' => 'center', // slide_align
+                'field_5731c6d886811' => '0', // slides_autoslide
+                'field_573d8880abc96' => 'center', // navigation_position
+                'field_58933fb6f5ed4' => array('wrapAround', 'pageDots'), // additional_options
+                'field_56a5e994398d6' => $images // slides
+            )
+        );
+
+        $moduleId = $this->save($data, $postId, $widgetData['widget_meta']['widget_id'], $widgetData['widget_meta']['sidebar']);
+    }
+}
+
+new \HbgMigrate\WidgetImageList();
