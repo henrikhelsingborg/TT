73a418afec66827e7548fabf55cca808cf3761a9
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-13 15:57:50 +0100

    Adds methods for saving image

--- wp-content/mu-plugins/hbg-migrate/Widget.php
+++ wp-content/mu-plugins/hbg-migrate/Widget.php
@@ -23,7 +23,7 @@ abstract class Widget
     /**
      * Saves migrated module
      * @param  array  $data The module data
-     * @return bool
+     * @return bool|int
      */
     public function save(array $data, int $postId, string $widgetId, string $sidebar)
     {
@@ -33,6 +33,12 @@ abstract class Widget
         // Bail if already migrated
         if ($isMigrated) {
             return false;
+        }
+
+        $acfFields = array();
+        if (isset($data['acf'])) {
+            $acfFields = $data['acf'];
+            unset($data['acf']);
         }
 
         $data['post_type'] = $this->moduleType;
@@ -60,11 +66,88 @@ abstract class Widget
 
         update_post_meta($postId, 'modularity-modules', $pageModules);
 
+        // Add acf fields
+        foreach ($acfFields as $key => $value) {
+            update_field($key, $value, $moduleId);
+        }
+
+        // Add widget to list of completed migrations
         $migrated[] = $widgetId;
         update_option('hbgmigrate_migrated_widgets', $migrated);
 
         echo 'Migrated widget <strong>"' . $widgetId . '"</strong> of type <strong>"' . $this->widgetType . '"</strong> for post with id <strong>"' . $postId . '"</strong><br>';
 
-        return true;
+        return $moduleId;
+    }
+
+    /**
+     * Uploads image to use in module
+     * @param  string $url    Image to upliad to media gallery (url)
+     * @param  int $postId    Post id of the module
+     * @return int            Attachment id
+     */
+    public function saveImageFromUrl(string $url, int $postId) : int
+    {
+        // Upload paths
+        $uploadDir = wp_upload_dir();
+        $uploadDir = $uploadDir['path'];
+
+        if (!is_dir($uploadDir)) {
+            throw new \Error('No upload folder!');
+        }
+
+        $filename = basename($url);
+
+        // Bail if image already exists in library
+        if ($attachmentId = $this->attatchmentExists($uploadDir . '/' . basename($filename))) {
+            return $attachmentId;
+        }
+
+        // Save file to server
+        $contents = file_get_contents($url);
+        $save = fopen($uploadDir . '/' . $filename, 'w');
+        fwrite($save, $contents);
+        fclose($save);
+
+        // Detect filetype
+        $filetype = wp_check_filetype($filename, null);
+
+        // Insert the file to media library
+        $attachmentId = wp_insert_attachment(
+            array(
+                'guid' => $uploadDir . '/' . basename($filename),
+                'post_mime_type' => $filetype['type'],
+                'post_title' => $filename,
+                'post_content' => '',
+                'post_status' => 'inherit'
+            ),
+            $uploadDir . '/' . $filename,
+            $postId
+        );
+
+        // Generate attachment meta
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+        $attachData = wp_generate_attachment_metadata($attachmentId, $uploadDir . '/' . $filename);
+        wp_update_attachment_metadata($attachmentId, $attachData);
+
+        return $attachmentId;
+    }
+
+    /**
+     * Checks if a attachment src already exists in media library
+     * @param  string $src Media url
+     * @return mixed
+     */
+    protected function attatchmentExists(string $src)
+    {
+        global $wpdb;
+        $query = "SELECT ID FROM {$wpdb->posts} WHERE guid = '$src'";
+        $id = $wpdb->get_var($query);
+
+        if (!empty($id) && $id > 0) {
+            return $id;
+        }
+
+        return false;
     }
 }
