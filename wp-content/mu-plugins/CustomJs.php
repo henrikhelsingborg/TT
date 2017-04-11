e5c84296bcd9dc1767fcf6c5f7cb2a4c3bdb74a8
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-04-11 11:02:52 +0200

    Add Google Tag Manager code to customjs

--- wp-content/mu-plugins/CustomJs.php
+++ wp-content/mu-plugins/CustomJs.php
@@ -5,16 +5,34 @@ add_action('acf/init', function () {
     $shouldUpdate = false;
 
     // Add Vergic code if missing
-    if (strpos($customjs, 'https://account.psplugin.com') < 0) {
+    if (strpos($customjs, '// --Vergic--') === false) {
         $shouldUpdate = true;
-        $customjs .= '(function (server, psID) {
-                var s = document.createElement(\'script\');
-                s.type = \'text/javascript\';
-                s.src = server + \'/\' + psID + \'/ps.js\';
-                document.getElementsByTagName(\'head\')[0].appendChild(s);
-            }(\'https://account.psplugin.com\', \'331F5271-4B0B-4625-BF08-4157F101DBFF\'));';
+        $customjs .= '
+
+// --Vergic--
+(function (server, psID) {
+    var s = document.createElement(\'script\');
+    s.type = \'text/javascript\';
+    s.src = server + \'/\' + psID + \'/ps.js\';
+    document.getElementsByTagName(\'head\')[0].appendChild(s);
+}(\'https://account.psplugin.com\', \'331F5271-4B0B-4625-BF08-4157F101DBFF\'));';
     }
 
+    // Add Vergic code if missing
+    if (strpos($customjs, '// --Google Tag Manager--') === false) {
+        $shouldUpdate = true;
+        $customjs .= '
+
+// --Google Tag Manager--
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
+new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
+\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,\'script\',\'dataLayer\',\'GTM-PVK49V\');
+        ';
+    }
+
+    // Update if needed
     if ($shouldUpdate) {
         update_field('custom_js_input', $customjs, 'option');
     }
