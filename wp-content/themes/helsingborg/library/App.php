a4fd6d0c88517dfbf56e2938307dde6772cfd690
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-04-24 15:20:40 +0200

    Updated broken reference.

--- wp-content/themes/helsingborg/library/App.php
+++ wp-content/themes/helsingborg/library/App.php
@@ -13,7 +13,8 @@ class App
     public function updateTemplate($template_path)
     {
         if ($post_type = get_post_type()) {
-            if (get_post_type_object($post_type)->hierarchical == true && $post_type_object->_builtin == false) {
+            $post_type_object = get_post_type_object($post_type);
+            if (is_object($post_type_object) && $post_type_object->hierarchical == true && $post_type_object->_builtin == false) {
                 $template_path = \Municipio\Helper\Template::locateTemplate('page');
             }
         }
