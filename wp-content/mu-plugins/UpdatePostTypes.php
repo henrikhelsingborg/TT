9245be3d2fec8551871254826e98581279847b72
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-04-25 09:03:31 +0200

    Hierarchical post type categories

--- wp-content/mu-plugins/UpdatePostTypes.php
+++ wp-content/mu-plugins/UpdatePostTypes.php
@@ -39,7 +39,7 @@ class UpdatePostTypes
         $taxonomies = array(array(
             'label'                => 'Kategorier',
             'slug'                 => 'kategorier',
-            'hierarchical'         => false,
+            'hierarchical'         => true,
             'connected_post_types' => array('nyheter'),
             'public'               => true,
             'show_ui'              => true
