52c5e0b39a314bbc06949778af06a8ab0b1a5dc6
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-04-28 12:59:29 +0200

    Dynamic prefix.

--- wp-content/mu-plugins/hbg-migrate/MigrationEngine.php
+++ wp-content/mu-plugins/hbg-migrate/MigrationEngine.php
@@ -141,7 +141,7 @@ class MigrationEngine
         }
 
         //Update meta keys to new prefix
-        $wpdb->query("UPDATE {$wpdb->dbname}.{$wpdb->usermeta} SET meta_key = REPLACE(meta_key, 'wp_', 'hbg_')");
+        $wpdb->query("UPDATE {$wpdb->dbname}.{$wpdb->usermeta} SET meta_key = REPLACE(meta_key, 'wp_', {$wpdb->base_prefix})");
 
         //Mark as done.
         update_option('hbgmigrate_moved_users', true);
