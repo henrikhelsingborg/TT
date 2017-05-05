e9e8ffa55a6f7f637a017277f9fc3c8f2c8d26c8
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-05-05 09:43:54 +0200

    Do not update user

--- config/plugins.php
+++ config/plugins.php
@@ -24,8 +24,8 @@ define('WEB_FONT', 'Roboto');
  * @var string
  */
  define('AD_INTEGRATION_URL', 'https://intranat.helsingborg.se/ad-api/');
- define('AD_UPDATE_NAME', true);
- define('AD_UPDATE_EMAIL', true);
+ define('AD_UPDATE_NAME', false);
+ define('AD_UPDATE_EMAIL', false);
  define('AD_SAVE_PASSWORD', false);
- define('AD_RANDOM_PASSWORD', true);
+ define('AD_RANDOM_PASSWORD', false);
  define('AD_USER_DOMAIN', 'helsingborg.se');
