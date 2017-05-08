1adbddca47d5f8544cce481f7c8f2a00790acf2c
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-08 10:24:56 +0200

    Revert "Google translate scripts"
    This reverts commit 842927b2149289503ed5dbce49e059d04fc179bf.

--- wp-content/themes/helsingborg/library/Theme/Enqueue.php
+++ wp-content/themes/helsingborg/library/Theme/Enqueue.php
@@ -7,7 +7,20 @@ class Enqueue
     public function __construct()
     {
         // Enqueue scripts and styles
+        add_action('wp_enqueue_scripts', array($this, 'style'));
         add_action('wp_enqueue_scripts', array($this, 'script'));
+    }
+
+    /**
+     * Enqueue styles
+     * @return void
+     */
+    public function style()
+    {
+        wp_register_style('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/css/hbg-prime.min.css', '', '1.0.0');
+        wp_enqueue_style('hbg-prime');
+
+        wp_enqueue_style('Helsingborg-css', get_stylesheet_directory_uri(). '/assets/dist/css/app.min.css', '', filemtime(get_stylesheet_directory() . '/assets/dist/css/app.min.css'));
     }
 
     /**
@@ -16,9 +29,9 @@ class Enqueue
      */
     public function script()
     {
-        wp_enqueue_script('google-translate', 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit', array(), '1.0', true);
-        wp_add_inline_script('google-translate', 'function googleTranslateElementInit() {new google.translate.TranslateElement({pageLanguage:"sv",autoDisplay:false,gaTrack:true,gaId:"UA-16678811-1"},"google-translate-element");}');
-        wp_enqueue_script('google-translate');
+        wp_register_script('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/js/hbg-prime.min.js', '', '1.0.0', true);
+        wp_enqueue_script('hbg-prime');
 
+        wp_enqueue_script('Helsingborg-js', get_stylesheet_directory_uri(). '/assets/dist/js/app.min.js', '', filemtime(get_stylesheet_directory() . '/assets/dist/js/app.min.js'), true);
     }
 }
