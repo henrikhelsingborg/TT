01b9752b5559396a6ef02417d37c45c71716765d
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-05-16 11:54:43 +0200

    Seo settings
    Remove title-tag support.

--- wp-content/themes/helsingborg/library/Theme/Seo.php
+++ wp-content/themes/helsingborg/library/Theme/Seo.php
@@ -1,35 +1,34 @@
 <?php
-
-namespace Helsingborg\Theme;
+namespace Municipio\Theme;
 
 class Seo
 {
-    const IMAGE_NAME = 'default-meta-image.jpg';
+    const IMAGE_NAME = 'default-meta-image.png';
 
     public function __construct()
     {
-        // Disable Wordpress automatic redirects. It's a bit too smart.
+        // Disable Wordpress automatic redirects. It's a bit too smart. We want 404s and not a
+        // somewhat random redirect.
         remove_action('template_redirect', 'redirect_canonical');
 
-        // Let SEO plugin set the title tag.
-        add_theme_support('title-tag');
-        add_filter('the_seo_framework_ogimage_output', array($this, 'default_image'), 10, 2);
-        add_filter('the_seo_framework_twitterimage_output', array($this, 'default_image'), 10, 2);
+        //Handla og:image data
+        add_filter('the_seo_framework_ogimage_output', array($this, 'defaultImage'), 10, 2);
+        add_filter('the_seo_framework_twitterimage_output', array($this, 'defaultImage'), 10, 2);
 
         // Remove plugin hidden comments.
         add_filter('the_seo_framework_indicator', '__return_false');
     }
 
-    public function fallback_image()
+    public function fallbackImage()
     {
         $upload_dir = wp_upload_dir();
         return $upload_dir['url'] . '/' . self::IMAGE_NAME;
     }
 
-    public function default_image($image = '', $page_id = 0)
+    public function defaultImage($image = '', $page_id = 0)
     {
         if (empty($page_id) || !has_post_thumbnail($page_id)) {
-            $image = esc_url($this->fallback_image());
+            $image = esc_url($this->fallbackImage());
         }
 
         return $image;
