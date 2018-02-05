fb35e2ef7397a0e2a16c4ce371fec7eff30fc76a
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-02-05 07:40:59 +0100

    Allow login when active directory is down

--- config/plugins.php
+++ config/plugins.php
@@ -26,8 +26,8 @@ define('WEB_FONT', 'Roboto');
  define('AD_INTEGRATION_URL', 'https://intranat.helsingborg.se/ad-api/');
  define('AD_UPDATE_NAME', true);
  define('AD_UPDATE_EMAIL', true);
- define('AD_SAVE_PASSWORD', false);
- define('AD_RANDOM_PASSWORD', true);
+ define('AD_SAVE_PASSWORD', true);
+ define('AD_RANDOM_PASSWORD', false);
  define('AD_USER_DOMAIN', 'helsingborg.se');
 
 /**
